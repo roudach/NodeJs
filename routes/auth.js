e101a242123636fff415afc90b68317b8541e31c
roudayna <roudaynacherif9@gmail.com>
2024-02-22 11:46:28 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,18 @@ router.get('/login',(req,res)=>{
     res.send([{id:1, name:"sachin"}, {id:2, name:"saurav"}])
 })
 
+router.post('/register', async (req, res)=>{
+    try{
+        const user = new User({username, password})= req.body;
+            await user.save();
+            res.status(201).send('User register successed');
+            
+    }catch(error){
+        res.status(400).send(error.message)
+
+    }
+})
+
 router.get('/register',(req,res)=>{
     res.sendFile(__dirname+'/auth.html')})
 module.exports=router;
